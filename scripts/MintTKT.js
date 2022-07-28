d539e1631291125719b6c003ceb3c1e93639efee
Talha Sajid <86770288+IamTalha-Sajid@users.noreply.github.com>
2022-07-28 15:33:20 +0500

    First Upload

---
+++ scripts/MintTKT.js
@@ -0,0 +1,29 @@
+require('dotenv').config();
+
+async function loadWeb3() {
+    if (window.ethereum) {
+        window.web3 = new Web3(window.ethereum);
+        window.ethereum.enable();
+    }
+}
+
+async function loadContract() {
+    let abi = require("./ABI/MintTKT.json");
+    let address = process.env.MAIN_TKT_CONTRACT_ADDRESS;
+    return await new window.web3.eth.Contract(abi, address);
+}
+async function getCurrentAccount() {
+    const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+    return accounts[0];
+}
+
+async function run() {
+    await loadWeb3();
+    window.contract = await loadContract();
+    const account = await getCurrentAccount();
+    let result = await window.contract.methods.create(process.env.IPFS_TKT).send({ from: account, value: 500000000000000000 });
+}
+
+if (typeof window.ethereum !== 'undefined') {
+    run()
+}
